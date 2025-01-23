f3f182ad21a568e95426e81531465a20074dd69b
blake-chambers-bam <daniel.chambers@bamtech.net>
2025-01-22 21:15:59 -0600

    Add project files.

---
+++ ConsoleApp.OutputDemo/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! My name is Daniel Chambers");
+Console.WriteLine("I am from United States!");
+Console.WriteLine("Anything you want to say.");
+
+Console.Write("Line 1");
+
+Console.Write("Line 2");
+
---
+++ ConsoleApp.VariablesDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Different datatypes
+/*
+    text - string
+    integers-int
+    decimal-double, float, decimal
+    logical - bool
+    
+ */
+
+string name = "Daniel Chambers";
+
+Console.WriteLine(name);
+Console.WriteLine("They call me " + name);
+Console.WriteLine($"I was given the name {name}");
+Console.WriteLine("I am {0}", name);
+
+int age = 33;
+int retirementYearsLeft = 32;
+int retirementAge = age + retirementYearsLeft;
+
+Console.WriteLine("My retirement age is " + retirementAge);
+Console.WriteLine("I am " + age + " Years old");
+
+bool isRetired = false;
+Console.WriteLine("Am I retired? " + isRetired);
+
+
